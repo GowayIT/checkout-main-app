f728fdeb9096b61b1ebadbec54c807a6cee0d063
Jain Joseph <jain.joseph@goway.com>
2026-04-02 16:20:09 -0400

    fileresizer4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.9/iframeResizer.contentWindow.min.js"></script>
     <style>
         body {
             font-family: Arial, sans-serif;
